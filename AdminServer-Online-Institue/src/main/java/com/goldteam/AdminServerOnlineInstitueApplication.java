b53539bb89ea057f431c89c90873a8807ca954ee
Gynaranjan <swaingyanaranjan77@gmail.com>
2026-01-15 12:29:15 +0530

    admin update

---
+++ AdminServer-Online-Institue/src/main/java/com/goldteam/AdminServerOnlineInstitueApplication.java
@@ -0,0 +1,17 @@
+package com.goldteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerOnlineInstitueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServerOnlineInstitueApplication.class, args);
+		System.out.println("admin server on");
+	}
+
+}
---
+++ AdminServer-Online-Institue/src/test/java/com/goldteam/AdminServerOnlineInstitueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerOnlineInstitueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
