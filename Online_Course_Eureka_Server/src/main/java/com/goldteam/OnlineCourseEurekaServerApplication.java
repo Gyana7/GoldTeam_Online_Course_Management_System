b4844b70556fd211e032ad7c3bce412c8492b3a5
Satyam Kumar <your-email@example.com>
2026-01-15 11:58:05 +0530

    Eureka Server Update

---
+++ Online_Course_Eureka_Server/src/main/java/com/goldteam/OnlineCourseEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.goldteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourseEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCourseEurekaServerApplication.class, args);
+	}
+
+}
